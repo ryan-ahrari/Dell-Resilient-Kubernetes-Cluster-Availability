58210111f71738073868cf0da25d83c086576099
ryan-ahrari <52223969+ryan-ahrari@users.noreply.github.com>
2022-05-23 14:41:31 -0700

    Update ssh.py
    Update to working ssh.py

--- code/ssh.py
+++ code/ssh.py
@@ -1,37 +1,76 @@
-# pip install paramiko
 import paramiko
+import time
 
 
-sourcePath =''
-destinationPath = ''
-#Client1
-c1 = paramiko.SSHClient()
-c1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-c1.connect("xxx.xxx.xxx.xxx",22,username=xxx,password='',timeout=4)
+def get_connection(ip, log, paswd):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip, username=log, password=paswd, timeout=100)
+    console = ssh.invoke_shell()
+    console.keep_this = ssh
+    print(console)
+    return console
 
-sftp = c1.open_sftp()
-sftp.put(sourcePath, destinationPath)
+def move_data_dir():
 
-#Client2
-c2 = paramiko.SSHClient()
-c2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-c2.connect("xxx.xxx.xxx.xxx",22,username=xxx,password='',timeout=4)
+    # Source info
+    ip_source = "192.168.7.164"
+    user_source = "generic"
+    pass_source = "generic"
 
-sftp = c2.open_sftp()
-sftp.put(sourcePath, destinationPath)
+    # Dest info
+    ip_dest = "192.168.7.163"
+    user_dest = user_source  # Dest/Source have same username
+    pass_dest = pass_source  # Dest/Source have same password
 
-#client3
-c3 = paramiko.SSHClient()
-c3.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-c3.connect("xxx.xxx.xxx.xxx",22,username=xxx,password='',timeout=4)
+    tar_file = "etcd.tar.gz" # Name of tar file
 
-sftp = c3.open_sftp()
-sftp.put(sourcePath, destinationPath)
+    commands = [
+            "sudo su",
+            pass_source,
+            "cd /root",
+            "cd data", # Change this line and line below if etcd mounted somewhere else
+            "cd myss",
+            "tar -cvzf "+tar_file+" .",
+            "sudo scp "+tar_file+" "+user_dest+"@"+ip_dest+":/home/"+user_dest,
+            pass_dest,
+            "ssh "+user_dest+"@"+ip_dest,
+            pass_dest,
+            "sudo su",
+            pass_dest,
+            "cd /root",
+            "cd data", # Change this line and line below if you want to mount etcd somewhere else
+            "cd myss",
+            "mv /home/"+user_dest+"/"+tar_file+" .",
+            "tar -xvf "+tar_file,
+            "rm -r "+tar_file,
+            "exit",
+            "exit",
+            "rm -r "+tar_file
+            ]
 
-#client4
-c4 = paramiko.SSHClient()
-c4.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-c4.connect("xxx.xxx.xxx.xxx",22,username=xxx,password='',timeout=4)
+    channel = get_connection(ip_source, user_source, user_dest)
+    index = 0 # Index to put in new commands
 
-sftp = c4.open_sftp()
-sftp.put(sourcePath, destinationPath)
+    for command in commands:
+
+        channel.send(command+"\n")
+        while not channel.recv_ready():
+            time.sleep(0.01)
+        time.sleep(0.5)
+        output = channel.recv(9999)
+        output = output.decode('utf-8')
+
+        print('Output: ', output)
+
+        # Handle creating new dir if necessary
+        if "No such file or directory" in output:
+            clist = command.split(" ")
+            commands.insert(index+1, ("mkdir "+clist[1]))
+            commands.insert(index+2, command)
+
+        time.sleep(0.5)
+        index += 1
+    channel.close()
+
+move_data_dir()
